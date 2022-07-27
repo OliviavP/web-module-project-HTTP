fff20af98f08ec5152c9b975d05c9982f8b256e8
OliviavP <livalossy@gmail.com>
2022-07-27 09:40:11 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,69 +1,73 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import MovieList from './components/MovieList'
+import Movie from './components/Movie'
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from './components/MovieHeader'
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import FavoriteMovieList from './components/FavoriteMovieList'
+import EditMovieForm from './components/EditMovieForm'
+import AddMovieForm from './components/AddMovieForm'
 
-import axios from 'axios';
+import axios from 'axios'
 
 const App = (props) => {
-  const [movies, setMovies] = useState([]);
-  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [movies, setMovies] = useState([])
+  const [favoriteMovies, setFavoriteMovies] = useState([])
 
-  useEffect(()=>{
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
-        setMovies(res.data);
+  useEffect(() => {
+    axios
+      .get('http://localhost:9000/api/movies')
+      .then((res) => {
+        setMovies(res.data)
       })
-      .catch(err => {
-        console.log(err);
-      });
-  }, []);
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
-  const deleteMovie = (id)=> {
+  const deleteMovie = (id) => {
+    setMovies(movies.filter((item) => item.id !== Number(id)))
   }
 
-  const addToFavorites = (movie) => {
-    
-  }
+  const addToFavorites = (movie) => {}
 
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" > HTTP / CRUD Module Project</span>
+      <nav className='navbar navbar-dark bg-dark'>
+        <span className='navbar-brand'> HTTP / CRUD Module Project</span>
       </nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
+      <div className='container'>
+        <MovieHeader />
+        <div className='row '>
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
+
           <Switch>
-            <Route path="/movies/edit/:id">
+            <Route path='/movies/edit/:id'>
+              <EditMovieForm setMovies={setMovies} />
+            </Route>
+            <Route path='movies/add'>
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
-            <Route path="/movies/:id">
-              <Movie/>
+            <Route path='/movies/:id'>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
-            <Route path="/movies">
-              <MovieList movies={movies}/>
+            <Route path='/movies'>
+              <MovieList movies={movies} />
             </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
+            <Route path='/'>
+              <Redirect to='/movies' />
             </Route>
           </Switch>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-
-export default App;
-
+export default App
